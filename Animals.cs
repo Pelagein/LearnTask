151bc7adbad58e0b4dfa6af7a4baa9eb07504af6
Александр <@NIGHTMARE>
2023-11-20 02:21:32 +0300

    Добавьте файлы проекта.

---
+++ Animals.cs
@@ -0,0 +1,36 @@
+﻿namespace LearnTask
+{
+    class Animals
+    {
+        public Animals(string Name)
+        {
+            this.Name = Name;
+        }
+        public string Name { get; set; }
+        virtual public void Roar()
+        {
+            Console.WriteLine($"{Name} кричит!");
+        }
+
+    }
+
+    class Cat : Animals
+    {
+        public Cat(string Name) :base(Name) { }
+        public override void Roar()
+        {
+            Console.WriteLine($"Кот {Name} мяукает !");
+        }
+
+    }
+
+    class Dog : Animals
+    {
+        public Dog(string Name) : base(Name) { }
+        public override void Roar()
+        {
+            Console.WriteLine($"Пёс {Name} гавкает !");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Linq;
+
+namespace LearnTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Animals animal = new Animals("Зверь");
+            Cat cat = new Cat("Тобиас");
+            Dog dog = new Dog("Тузик");
+
+            animal.Roar();
+            dog.Roar();
+            cat.Roar();
+
+        }
+    }
+
+}
