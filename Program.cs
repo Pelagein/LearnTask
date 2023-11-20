0b9c4af8e5ce5e3efb3a6a0c90a139a27a55cf7c
Pelagein <pelagein.official@yandex.ru>
2023-11-20 03:26:48 +0300

    Создали массив Shapes (фигур) и заполнили его объектами классов Triangle, Rectangle

--- Program.cs
+++ Program.cs
@@ -1,24 +1,36 @@
-﻿using System.Xml.Linq;
-
-namespace LearnTask
+﻿namespace LearnTask
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
-            Animals animal = new Animals("Зверь");
-            Cat cat = new Cat("Тобиас");
-            Dog dog = new Dog("Тузик");
+            // Повторения Наследования.
+            //Animals animal = new Animals("Зверь");
+            //Cat cat = new Cat("Тобиас");
+            //Dog dog = new Dog("Тузик");
 
-            animal.Roar();
-            dog.Roar();
-            cat.Roar();
+            //animal.Roar();
+            //dog.Roar();
+            //cat.Roar();
 
-            Animals catTwo = new Cat("Василий");
-            catTwo.Roar();
+            //Animals catTwo = new Cat("Василий");
+            //catTwo.Roar();
+
+            //Повторение полиморфизма (абстрактных классов)
+
+            Shape[] shapes = new Shape[2];
+            shapes[0] = new Triangle(12, 24, 13);
+            shapes[1] = new Rectangle(10, 23);
+
+            foreach (Shape shape in shapes)
+            {
+                Console.WriteLine(shape.Area());
+                Console.WriteLine(shape.Perimetr());
+            }
+            
 
         }
+
     }
 
 }
