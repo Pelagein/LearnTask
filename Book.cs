fc2319ad48e5d53ee615eb543ff838ff9db61477
Pelagein <pelagein.official@yandex.ru>
2023-11-22 04:09:16 +0300

    Выполнил пару тестовых задачек.

---
+++ Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnTask
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+        public bool IsRead { get; private set; }
+
+        public Book(string title, string author, int publicationYear)
+        {
+            Title = title;
+            Author = author;
+            PublicationYear = publicationYear;
+            IsRead = false;
+    
+    }
+
+        public void MarkAsRead()
+        {
+            IsRead = true;
+        }
+
+        public override string ToString()
+        {
+            return ($"{Title} автора {Author} - {IsRead}");
+        }
+
+        public void Print()
+        {
+            if (IsRead) Console.WriteLine($"{Title} автора {Author} - Прочитно");
+            else Console.WriteLine($"{Title} автора {Author}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -18,16 +18,52 @@
 
             //Повторение полиморфизма (абстрактных классов)
 
-            Shape[] shapes = new Shape[2];
-            shapes[0] = new Triangle(12, 24, 13);
-            shapes[1] = new Rectangle(10, 23);
+            //Shape[] shapes = new Shape[2];
+            //shapes[0] = new Triangle(12, 24, 13);
+            //shapes[1] = new Rectangle(10, 23);
 
-            foreach (Shape shape in shapes)
+            //foreach (Shape shape in shapes)
+            //{
+            //    Console.WriteLine(shape.Area());
+            //    Console.WriteLine(shape.Perimetr());
+            //}
+
+            //Test Task 
+
+            //Book book = new Book("Programming in C", "John Doe", 2023);
+            //book.Print();
+            //book.MarkAsRead();
+            //book.Print();
+
+            //Задача 1.
+            //Дан массив целых чисел. Требуется сформировать новый массив без дубликатов и вывести результат в консоль.
+            //Пример: (i.e. "1 3 5 3 7 3 1 1 5"-> "1 3 5 7")
+            //В алгоритме нельзя использовать System.Linq.
+            //*/
+            var arr = new int[] { 1, 3, 5, 3, 7, 3, 1, 1, 5 };
+            var list = new List<int>();
+
+            for (int i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine(shape.Area());
-                Console.WriteLine(shape.Perimetr());
+                int n = 0;
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (arr[i] == list[j]) { n++; }
+                }
+                if (n == 0) { list.Add(arr[i]); }
             }
-            
+            foreach (int k in list) { Console.Write(k); }
+
+            var array = new List<int>();
+
+            array = arr.Distinct().ToList();
+            foreach (int k in list) { Console.Write(k); }
+            //foreach (int i in arr)
+            //{
+            //    Console.Write($"{i} ");
+            //}
+
+
 
         }
 
